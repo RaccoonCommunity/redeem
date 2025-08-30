5f524c244edd831e3067af1b28a9d3537a180251
RaccoonCommunity <rc.community2022@gmail.com>
2025-08-30 20:15:06 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ def selenium_api_check(data):
     used_orders = load_used_orders()
 
     options = Options()
-    # options.add_argument('--headless')
+    options.add_argument('--headless')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
     options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
